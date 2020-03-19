20cf51cfeddb4cd1a2541765e2c096b86d1d23cb
Wofluently <f_liuchang@163.com>
2020-03-19 11:43:33 +0800

    Little compnent

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -9,6 +9,7 @@ import NumberList from "../component/List";
 import Form from "../component/Form";
 import ImproveStatus from "../component/ImproveStatus";
 import Combination from "../component/Combination";
+import LittleProject from "../component/LittleProject";
 
 const Main = () => {
   const comment = {
@@ -20,8 +21,48 @@ const Main = () => {
     date: new Date()
   };
   const numbers = [1, 2, 3, 4, 5];
+
+  const PRODUCTS = [
+    {
+      category: "Sporting Goods",
+      price: "$49.99",
+      stocked: true,
+      name: "Football"
+    },
+    {
+      category: "Sporting Goods",
+      price: "$9.99",
+      stocked: true,
+      name: "Baseball"
+    },
+    {
+      category: "Sporting Goods",
+      price: "$29.99",
+      stocked: false,
+      name: "Basketball"
+    },
+    {
+      category: "Electronics",
+      price: "$99.99",
+      stocked: true,
+      name: "iPod Touch"
+    },
+    {
+      category: "Electronics",
+      price: "$399.99",
+      stocked: false,
+      name: "iPhone 5"
+    },
+    {
+      category: "Electronics",
+      price: "$199.99",
+      stocked: true,
+      name: "Nexus 7"
+    }
+  ];
   return (
     <div>
+      <LittleProject products={PRODUCTS}></LittleProject>
       <Combination></Combination>
       <ImproveStatus></ImproveStatus>
       <Form></Form>
